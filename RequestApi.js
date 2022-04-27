561da41213d67ffa9af9f2a520c63746c1be4258
Marco <marcoxd01@hotmail.com>
2022-04-27 15:35:09 -0300

    testeantoniel

--- RequestApi.js
+++ RequestApi.js
@@ -1,13 +1,19 @@
+// function montaRequest(moedasComparadas) {
+//   let request = new XMLHttpRequest();
+//   request.onload = function () {
+//     console.log(this);
+//     let data = JSON.parse(this.responseText);
+//     mostrarResultado(data);
+//   };
+//   request.open(
+//     "GET",
+//     `https://economia.awesomeapi.com.br/json/last/${moedasComparadas}`
+//   );
+//   request.send();
+// }
+
 function montaRequest(moedasComparadas) {
-  let request = new XMLHttpRequest();
-  request.onload = function () {
-    let data = JSON.parse(this.responseText);
-    console.log(data);
-    mostrarResultado(data);
-  };
-  request.open(
-    "GET",
-    `https://economia.awesomeapi.com.br/json/last/${moedasComparadas}`
-  );
-  request.send();
+  fetch(`https://economia.awesomeapi.com.br/json/last/${moedasComparadas}`)
+    .then((response) => response.json())
+    .then((data) => mostrarResultado(data));
 }
